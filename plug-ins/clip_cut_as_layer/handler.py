e524a877f2755ff130898d539860149aa9a62b10
Raghav <raghav196.rs@gmail.com>
2025-05-17 13:30:26 +0530

    plugin: clipboard enhancements

--- plug-ins/clip_cut_as_layer/handler.py
+++ plug-ins/clip_cut_as_layer/handler.py
@@ -15,6 +15,12 @@ def run_one(
         config: Gimp.ProcedureConfig,
         data):
 
+
+
+    # has_selection = not Gimp.Selection.is_empty(image)
+    _, has_selection, x1, y1, x2, y2 = Gimp.Selection.bounds(image)
+    # Gimp.message(f"Selection: bounds: {has_selection}, x1: {x1}, y1: {y1}, x2: {x2}, y2: {y2}")
+
     image.undo_group_start()
 
     Gimp.edit_cut([drawable])
@@ -23,6 +29,9 @@ def run_one(
     Gimp.floating_sel_to_layer(floating_sel)
 
     image.set_selected_layers([drawable])
+
+    if has_selection:
+        image.select_rectangle(Gimp.ChannelOps.REPLACE, x1, y1, x2-x1, y2-y1)
 
     image.undo_group_end()
     return gimp_error.success(procedure)
